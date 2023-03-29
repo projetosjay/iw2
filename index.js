96b6df45eb5bc054960fd73a21235c6e3e6fc06a
jayane_santos <100379675+projetosjay@users.noreply.github.com>
2023-03-29 10:43:58 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function formulario(){
     window.location.href= 'formulario.html';
 }
+
+function home(){
+    window.location.href= 'index.html';
+}
+
+function graduacao(){
+    window.location.href = 'graduacao.html';
+}
